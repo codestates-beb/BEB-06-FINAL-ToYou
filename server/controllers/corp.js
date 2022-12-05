2842c2372f4fd7316d9c54c36cab192e8ec33191
yunseho <sehosla@naver.com>
2022-12-05 17:50:23 +0900

    스타트업

--- server/controllers/corp.js
+++ server/controllers/corp.js
@@ -1,18 +1,22 @@
-const { board } = require("../models/board");
+const { corp } = require("../models/corp");
 const moment = require('moment');
 const date= moment().format('YYYY-MM-DD HH:mm:ss');
 
-exports.PostImage = (req, res, next) => {
+exports.CorpImage = (req, res, next) => {
     console.log(req.body)
     const post = {
-      ID: req.body.ID,
+      type: req.body.type,
+      name: req.body.name,
+      corp: req.body.corp,
       title: req.body.title,
-      content: req.body.content,
+      body: req.body.body,
       image:req.file.path,
-      exp: req.file.exp,
+      since: req.body.since,
+      zone: req.body.zone,
+      up:req.body.up,
       createAT: date
     };
-    const Submit = new board(post)
+    const Submit = new corp(post)
     Submit.save((err,doc)=>{
       if(err) return res.status(404).json({fail: false, err})
       return res.status(200).json({success:true})
--- server/models/corp.js
+++ server/models/corp.js
@@ -1,1 +1,62 @@
 //스타트업 페이지
+const mongoose = require('mongoose');
+const moment = require('moment');
+const { Schema } = require('mongoose');
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+const date= moment().format('YYYY-MM-DD HH:mm:ss');
+const schema = mongoose.Schema;
+
+const corpSchema = new schema({
+     id:{
+        type: Schema.Types.ObjectId,  
+        ref:'user',
+        require: true
+    },
+    type:{
+        type:String,
+        
+    },
+    name:{
+        type:String,
+       
+    },
+    corp:{
+        type:String
+    },
+    symbol:{
+        type:String
+    },
+    image:[{
+        type:String
+    }],
+    body: {
+        type: String
+    },
+    since: {
+        type: String
+    },
+    zone: {
+        type: String
+    },
+    upz:[{type: Schema.Types.ObjectId, ref:'user'}],
+    createAT:{
+        type:String,
+        default: date,
+    },
+})
+
+// {
+//     "type": "sad",
+//     "name" : "asd",
+//     "corp" : "sda/sda",
+//     "symbol" : "asd",
+//     "image" : "sda",
+//     "body" : "sad sad",
+//     "since" : "1",
+//     "zone" : "seou."
+//     }
+corpSchema.plugin(AutoIncrement, { inc_field: "num" });
+
+const corp = mongoose.model('corp',corpSchema)
+
+module.exports={corp}
